423534f74926ef017daa7b9ac55fd51fcfb06975
Sebastian <ss@kvisa.de>
2022-12-30 00:43:38 +0100

    v 1.0.0.3

--- LessMotorBrakeForce.lua
+++ LessMotorBrakeForce.lua
@@ -5,6 +5,7 @@
 -- @date        23/12/2022
 -- @info        LessMotorBrakeforce Script for FarmingSimulator 2022
 -- changed		app to pre 23.12.2022 SbSh(s4t4n)
+-- changelog	Anpassung an FS22_realismAddon_gearbox von modelleicher
 
 LessMotorBrakeforce = {};
 
@@ -30,13 +31,28 @@ function LessMotorBrakeforce:onUpdate(dt)
 			if self.spec_motorized.motor ~= nil then
 				if self.spec_motorized.motor.lowBrakeForceScale ~= nil then
 					-- 0.25 = -25% BrakeForce
-					self.spec_motorized.motor.lowBrakeForceScale = self.spec_motorized.motor.lowBrakeForceScale*0.45; -- here you can change the engine brake effect.
-																													  -- in next version you'll change via ingame hud
+					if self.spec_motorized.motor.lastManualShifterActive == false then
+						self.spec_motorized.motor.lowBrakeForceScale = self.spec_motorized.motor.lowBrakeForceScale*0.52 
+						self.spec_motorized.motor.lowBrakeForceSpeedLimit = 0.00427 -- 15 km/h
+						-- Fahrstufe I.
+						-- self.spec_motorized.motor.gearRatio = self.spec_motorized.motor.gearRatio * 1.5
+						
+						
+						-- Fahrstufe II.
+						
+					end
+					if self.spec_motorized.motor.lastManualShifterActive == true then
+						-- self.spec_motorized.motor.lowBrakeForceScale = 0.04; 
+					end
 				end;
 			end;
 		end;
 	end;
+	g_currentMission:addExtraPrintText("gearRatio: " .. self.spec_motorized.motor.gearRatio)
+	-- g_currentMission:addExtraPrintText("gearChangeTimeAutoReductionTime: " .. self.spec_motorized.motor.gearChangeTimeAutoReductionTime)
+	-- g_currentMission:addExtraPrintText("differentialRotSpeed: " .. self.spec_motorized.motor.differentialRotSpeed*math.pi)
+	-- g_currentMission:addExtraPrintText("rawLoadPercentage: " .. self.spec_motorized.motor.rawLoadPercentage)
+	
 end;
-Drivable = true
-Drivable.onUpdate  = Utils.prependedFunction(Drivable.onUpdate, LessMotorBrakeforce.onUpdate);
-
+-- Drivable = true
+Drivable.onUpdate  = Utils.appendedFunction(Drivable.onUpdate, LessMotorBrakeforce.onUpdate);

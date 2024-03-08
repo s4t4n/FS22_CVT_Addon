2b6d466012bb6089a9bce7a8c30b8d6834935bea
SbSh Sebastian <ss@kvisa.de>
2024-03-08 17:01:38 +0100

    1.0.0.02
    - CVT_Addon.lua:1993-1995: attempt to index a nil value

--- FS22_CVT_Addon/registerCVT_Addon.lua
+++ FS22_CVT_Addon/registerCVT_Addon.lua
@@ -12,19 +12,31 @@ LMBFRegister.done = false
 local specName = g_currentModName..".CVTaddon"
 
 if g_specializationManager:getSpecializationByName("CVTaddon") == nil then
-  	g_specializationManager:addSpecialization("CVTaddon", "CVTaddon", g_currentModDirectory.."CVT_Addon.lua", nil)
+	print("CVTa ModName: ".. g_currentModName)
+	if g_currentModName ~= "FS22_CVT_Addon" then
+		if string.find(tostring(g_currentModName), "master") or string.find(tostring(g_currentModName), "main") then
+			print("Please download the Github Version from the Releases or repack it correctly. Description at the Wiki.")
+			print("Or otherwise, download it from the official Modhub.")
+		else
+			print("Wrong Modname ".. g_currentModName .. " or you downloaded from a website that has stolen the mod")
+			print("and did an illegal upload there!")
+			print("Keep the original download-link from official Giants Modhub!")
+			print("Thank You.")
+		end
+		LMBFRegister.done = false
+		-- break;
+	else
+		g_specializationManager:addSpecialization("CVTaddon", "CVTaddon", g_currentModDirectory.."CVT_Addon.lua", nil)
+		LMBFRegister.done = true
+	end
 end
 
 for typeName, typeEntry in pairs(g_vehicleTypeManager.types) do
     if
 		SpecializationUtil.hasSpecialization(Motorized, typeEntry.specializations)
-	-- or SpecializationUtil.hasSpecialization(Vehicles, typeEntry.specializations)
-	-- or SpecializationUtil.hasSpecialization(Cylindered, typeEntry.specializations)
-    
     and not SpecializationUtil.hasSpecialization(Locomotive, typeEntry.specializations)
     and not SpecializationUtil.hasSpecialization(ConveyorBelt, typeEntry.specializations)
-    
-    then
+     then
      	g_vehicleTypeManager:addSpecialization(typeName, specName)
     end
 end
